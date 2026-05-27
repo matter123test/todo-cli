6c0664fade197fc898529e6747bdbc73197209db
matter123test <84776941+matter123test@users.noreply.github.com>
2026-05-27 23:37:00 +0200

    Added coloring

--- task/print.go
+++ task/print.go
@@ -5,21 +5,29 @@ import (
 	"os"
 	"text/tabwriter"
 	"time"
+
+	"github.com/fatih/color"
 )
 
 func (manager *TaskManager) Print() {
 	w := tabwriter.NewWriter(os.Stdout, 0, 0, 3, ' ', 0)
 
-	// Format header with tabs separating columns
-	fmt.Fprintln(w, "ID\tCONTENTS\tCREATED")
+	idColor := color.New(color.FgYellow).SprintFunc()
+	contentsColor := color.New(color.FgWhite).SprintFunc()
+	durationColor := color.New(color.FgMagenta).SprintFunc()
 
-	// Iterate and print data
+	// Header
+	fmt.Fprintf(w, "%s\t%s\t%s\n", color.CyanString("ID"), color.CyanString("CONTENTS"), color.CyanString("CREATED"))
+
 	for _, task := range manager.tasks {
-		createdAt := task.created
-		duration := time.Unix(createdAt, 0)
-		since := time.Since(duration).Truncate(time.Second)
+		duration := time.Unix(task.created, 0)
+		since := time.Since(duration).Truncate(time.Second).String()
 
-		fmt.Fprintf(w, "%v\t%v\t%v\n", task.id, task.contents, since)
+		fmt.Fprintf(w, "%v\t%v\t%v\n",
+			idColor(task.id),
+			contentsColor(task.contents),
+			durationColor(since),
+		)
 	}
 
 	w.Flush()
